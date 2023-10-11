d6c3b6fa33ed0abd09f13c1a1901e018341fe8df
808Mak1r <39427457+808Mak1r@users.noreply.github.com>
2023-10-11 15:28:04 +0800

    feat: 给 gin 设置静态文件路由和默认路由匹配

--- main.go
+++ main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.Writer.Write([]byte("Hello World"))
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
 		router.Run(":8080")
 	}()
 
-	_ = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	macChromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 	macEdgePath := "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"
-	cmd := exec.Command(macEdgePath, "--app=http://127.0.0.1:8080/")
+	_ = macChromePath
+	cmd := exec.Command(macEdgePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()
 
 	chSignal := make(chan os.Signal, 1)
