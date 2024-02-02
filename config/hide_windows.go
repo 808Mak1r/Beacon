a86fbeea6504c4b0ce02d61bfefa9ea2d0974fa4
808Mak1r <39427457+808Mak1r@users.noreply.github.com>
2024-02-02 11:49:42 +0800

    fix: 修复hide

--- config/hide_windows.go
+++ config/hide_windows.go
@@ -17,7 +17,7 @@ func showConsoleAsync() {
 	if console != 0 {
 		_, processID := w32.GetWindowThreadProcessId(console)
 		myProcessID := os.Getpid()
-		if processID == myProcessID {
+		if int(processID) == myProcessID {
 			w32.ShowWindow(console, w32.SW_HIDE)
 		}
 	}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/gonutz/w32/v2 v2.11.1
 	github.com/google/uuid v1.3.1
 	github.com/gorilla/websocket v1.5.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
